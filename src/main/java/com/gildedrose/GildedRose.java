eb86cb9f235b214373cbcdcd5f4dbc3cccf4623b
Guillaume LASNE <guillaume.lasne@lacombedulionvert.fr>
2019-08-23 10:35:55 +0200

    Isolate Sulfuras and encapsulate primitives

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,7 @@ class GildedRose {
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     public static final String AGED_BRIE = "Aged Brie";
     public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final int MAXIMUM_QUALITY = 50;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -17,53 +18,70 @@ class GildedRose {
     }
 
     private void updateOneItemQuality(Item item) {
-        if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACKSTAGE_PASSES)) {
-            if (item.quality > 0) {
-                if (!item.name.equals(SULFURAS)) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+        if (SULFURAS.equals(item.name)) {
+            return;
+        }
+        if (item.name.equals(AGED_BRIE)
+                || item.name.equals(BACKSTAGE_PASSES)) {
+            if (item.quality < MAXIMUM_QUALITY) {
+                increaseQuality(item);
 
                 if (item.name.equals(BACKSTAGE_PASSES)) {
                     if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                        increaseQualitySafely(item);
                     }
 
                     if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                        increaseQualitySafely(item);
                     }
                 }
+            }
+        } else {
+            if (item.quality > 0) {
+                decreaseQuality(item);
             }
         }
 
         if (!item.name.equals(SULFURAS)) {
-            item.sellIn = item.sellIn - 1;
+            decreaseSellIn(item);
         }
 
         if (item.sellIn < 0) {
-            if (!item.name.equals(AGED_BRIE)) {
-                if (!item.name.equals(BACKSTAGE_PASSES)) {
+            if (item.name.equals(AGED_BRIE)) {
+                increaseQualitySafely(item);
+            } else {
+                if (item.name.equals(BACKSTAGE_PASSES)) {
+                    setQualityToZero(item);
+                } else {
                     if (item.quality > 0) {
                         if (!item.name.equals(SULFURAS)) {
-                            item.quality = item.quality - 1;
+                            decreaseQuality(item);
                         }
                     }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
                 }
             }
         }
+    }
+
+    private void setQualityToZero(Item item) {
+        item.quality = 0;
+    }
+
+    private void decreaseSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private void increaseQuality(Item item) {
+        item.quality = item.quality + 1;
+    }
+
+    private void increaseQualitySafely(Item item) {
+        if (item.quality < MAXIMUM_QUALITY) {
+            increaseQuality(item);
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        item.quality = item.quality - 1;
     }
 }
